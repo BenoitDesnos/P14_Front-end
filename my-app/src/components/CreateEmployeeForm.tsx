25a925fda01ce7a10fa82c3d753d89708092cd36
Benoît DESNOS <benoit@season-ed.com>
2024-02-27 01:25:00 +0100

    fix type bug

--- my-app/src/components/CreateEmployeeForm.tsx
+++ my-app/src/components/CreateEmployeeForm.tsx
@@ -68,7 +68,7 @@ export function CreateEmployeeForm() {
       street: "",
       city: "",
       state: "state",
-      "zip-code": 0,
+      "zip-code": undefined,
       department: "department",
     },
   });
